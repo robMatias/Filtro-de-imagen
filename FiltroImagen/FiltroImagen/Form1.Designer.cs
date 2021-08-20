f0b14440d8ce8b58c1ae775765add4b92a8c4b74
Servando Matias <servandokratos@gmail.com>
2021-08-20 14:50:20 -0500

    Add files via upload

---
+++ FiltroImagen/FiltroImagen/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace FiltroImagen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirImagenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarImagenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filtrosBasicosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.negativoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filtroDeColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tonosDeGrisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorizarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colonizarGradianteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.brilloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.filtrosBasicosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1137, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirImagenToolStripMenuItem,
+            this.guardarImagenToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirImagenToolStripMenuItem
+            // 
+            this.abrirImagenToolStripMenuItem.Name = "abrirImagenToolStripMenuItem";
+            this.abrirImagenToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.abrirImagenToolStripMenuItem.Text = "Abrir Imagen";
+            this.abrirImagenToolStripMenuItem.Click += new System.EventHandler(this.abrirImagenToolStripMenuItem_Click);
+            // 
+            // guardarImagenToolStripMenuItem
+            // 
+            this.guardarImagenToolStripMenuItem.Name = "guardarImagenToolStripMenuItem";
+            this.guardarImagenToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.guardarImagenToolStripMenuItem.Text = "Guardar Imagen";
+            this.guardarImagenToolStripMenuItem.Click += new System.EventHandler(this.guardarImagenToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // filtrosBasicosToolStripMenuItem
+            // 
+            this.filtrosBasicosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripMenuItem,
+            this.negativoToolStripMenuItem,
+            this.filtroDeColorToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.tonosDeGrisToolStripMenuItem,
+            this.colorizarToolStripMenuItem,
+            this.colonizarGradianteToolStripMenuItem,
+            this.brilloToolStripMenuItem});
+            this.filtrosBasicosToolStripMenuItem.Name = "filtrosBasicosToolStripMenuItem";
+            this.filtrosBasicosToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.filtrosBasicosToolStripMenuItem.Text = "FIltros";
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.testToolStripMenuItem.Text = "Test";
+            this.testToolStripMenuItem.Click += new System.EventHandler(this.testToolStripMenuItem_Click);
+            // 
+            // negativoToolStripMenuItem
+            // 
+            this.negativoToolStripMenuItem.Name = "negativoToolStripMenuItem";
+            this.negativoToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.negativoToolStripMenuItem.Text = "Negativo";
+            this.negativoToolStripMenuItem.Click += new System.EventHandler(this.negativoToolStripMenuItem_Click);
+            // 
+            // filtroDeColorToolStripMenuItem
+            // 
+            this.filtroDeColorToolStripMenuItem.Name = "filtroDeColorToolStripMenuItem";
+            this.filtroDeColorToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.filtroDeColorToolStripMenuItem.Text = "Filtro de color rojo";
+            this.filtroDeColorToolStripMenuItem.Click += new System.EventHandler(this.filtroDeColorToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(193, 22);
+            this.toolStripMenuItem2.Text = " Aberracion Cromatica";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // tonosDeGrisToolStripMenuItem
+            // 
+            this.tonosDeGrisToolStripMenuItem.Name = "tonosDeGrisToolStripMenuItem";
+            this.tonosDeGrisToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.tonosDeGrisToolStripMenuItem.Text = "Tonos de gris";
+            this.tonosDeGrisToolStripMenuItem.Click += new System.EventHandler(this.tonosDeGrisToolStripMenuItem_Click);
+            // 
+            // colorizarToolStripMenuItem
+            // 
+            this.colorizarToolStripMenuItem.Name = "colorizarToolStripMenuItem";
+            this.colorizarToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.colorizarToolStripMenuItem.Text = "Colorizar";
+            this.colorizarToolStripMenuItem.Click += new System.EventHandler(this.colorizarToolStripMenuItem_Click);
+            // 
+            // colonizarGradianteToolStripMenuItem
+            // 
+            this.colonizarGradianteToolStripMenuItem.Name = "colonizarGradianteToolStripMenuItem";
+            this.colonizarGradianteToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.colonizarGradianteToolStripMenuItem.Text = "Colonizar Gradiante";
+            this.colonizarGradianteToolStripMenuItem.Click += new System.EventHandler(this.colonizarGradianteToolStripMenuItem_Click);
+            // 
+            // brilloToolStripMenuItem
+            // 
+            this.brilloToolStripMenuItem.Name = "brilloToolStripMenuItem";
+            this.brilloToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.brilloToolStripMenuItem.Text = "Brillo ";
+            this.brilloToolStripMenuItem.Click += new System.EventHandler(this.brilloToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Imagenes PNG|*.png|Imagenes BitMap|*.bmp";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Imagenes PNG|*.png";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 762);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Aplicador de filtros";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirImagenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarImagenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filtrosBasicosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem negativoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filtroDeColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem tonosDeGrisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorizarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colonizarGradianteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem brilloToolStripMenuItem;
+    }
+}
+
---
+++ FiltroImagen/FiltroImagen/Form1.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiltroImagen
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap original;
+        private Bitmap resultante;
+        private int[] histograma = new int[256];
+        private int[,] conv3x3 = new int[3, 3];
+        private int factor;
+        private int offset;
+
+        //Variables para double buffer y evitar el flicker
+
+        private int anchoVentana, altoVentana;
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Cerramos la aplicacion
+            this.Close();
+        }
+
+        private void abrirImagenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                original = (Bitmap)(Bitmap.FromFile(openFileDialog1.FileName));
+                anchoVentana = original.Width;
+                altoVentana = original.Height;
+
+                resultante = original;
+
+                this.Invalidate();
+            }
+        }
+
+        private void guardarImagenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                resultante.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Verificamos que se tenga un Bitmap instanciado
+            if (resultante != null)
+            {
+                // Obtenemos el objeto graphics
+                Graphics g = e.Graphics;
+
+                // Calculamos el scroll
+                AutoScrollMinSize = new Size(anchoVentana, altoVentana);
+
+                // Copiamos el bitmap a la ventana
+                g.DrawImage(resultante,
+                            new Rectangle(this.AutoScrollPosition.X,
+                                           this.AutoScrollPosition.Y + 30, 
+                                           anchoVentana, altoVentana));
+
+                // Liberamos el recurso
+                g.Dispose(); 
+            }
+        }
+
+        private void testToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            int y = 0;
+
+            resultante = new Bitmap(original.Width, original.Height);
+
+            for (x = 0; x < original.Width; x++)
+            {
+                for (y = 0; y < original.Height; y++)
+                {
+                    resultante.SetPixel(x, y, Color.FromArgb(128, 0, 0));
+                }
+            }
+
+            this.Invalidate();
+        }
+
+        private void negativoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // invertir la imagen, saca su negativo
+
+            int x = 0;
+            int y = 0;
+            resultante = new Bitmap(original.Width, original.Height);
+            Color rColor = new Color();
+            Color oColor = new Color();
+
+
+            for (x = 0; x < original.Width; x++)
+            {
+                for (y = 0; y < original.Height; y++)
+                {
+                    // Obtenemos el color del pixel
+                    oColor = original.GetPixel(x, y);
+
+                    // Procesamos y obtenemos el nuevo color
+                    rColor = Color.FromArgb(255 - oColor.R, 255 - oColor.G, 255 - oColor.B);
+
+                    // Colocamos el color en resultante
+                    resultante.SetPixel(x, y, rColor);
+                }
+            }
+
+            this.Invalidate();
+        }
+
+        private void filtroDeColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Filtro de color, solo presenta pixeles en un componente y elimina los otros dos
+            int x = 0;
+            int y = 0;
+            resultante = new Bitmap(original.Width, original.Height);
+            Color rColor = new Color();
+            Color oColor = new Color();
+
+            for (x = 0; x < original.Width; x++)
+            {
+                for (y = 0; y < original.Height; y++)
+                {
+                    // Obtenemos el color del pixel
+                    oColor = original.GetPixel(x, y);
+
+                    // Procesamos y obtenemos el nuevo color
+                    rColor = Color.FromArgb(oColor.R, 0, 0);
+
+                    // Colocamos el color resultante
+                    resultante.SetPixel(x, y, rColor);
+                }
+            }
+
+            this.Invalidate();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            int y = 0;
+            int a = 5; // Tamaño de la aberración
+
+            int r = 0;
+            int g = 0;
+            int b = 0;
+
+            resultante = new Bitmap(original.Width, original.Height);
+
+            for (x = 0; x < original.Width; x++)
+            {
+                for (y = 0; y < original.Height; y++)
+                {
+                    // Obtenemos el verde 
+                    g = original.GetPixel(x, y).G;
+
+                    // Obtenemos el rojo
+                    if ((x + a) < original.Width)
+                    {
+                        r = original.GetPixel(x + a, y).R;
+                    }
+                    else
+                    {
+                        r = 0;
+                    }
+
+                    // Obtenemos el azul
+                    if ((x - a) >= 0)
+                    {
+                        b = original.GetPixel(x - a, y).B;
+                    }
+                    else
+                    {
+                        b = 0;
+                    }
+
+                    // Colocamos el pixel 
+                    resultante.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            this.Invalidate();
+        }
+
+        private void tonosDeGrisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Convertimos la imagen a tonos de gris
+
+            int x = 0;
+            int y = 0;
+            resultante = new Bitmap(original.Width, original.Height);
+            Color rColor = new Color();
+            Color oColor = new Color();
+
+            float g = 0;
+
+            for (x = 0; x < original.Width; x++)
+            {
+                for (y = 0; y < original.Height; y++)
+                {
+                    // Obtenemos el color del pixel
+                    oColor = original.GetPixel(x, y);
+
+                    // Procesamos y obtenemos el numero de color
+
+                    g = oColor.R * .299f + oColor.G * 0.587f + oColor.B * 0.114f;
+
+                    rColor = Color.FromArgb((int)g, (int)g, (int)g);
+
+                    resultante.SetPixel(x, y, rColor);
+                }
+            }
+
+            this.Invalidate();
+        }
+
+        private void colorizarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            int y = 0;
+
+            // Queremos colorizar con (120,200,120)
+            double rc = 120 / 255.0;
+            double gc = 200 / 255.0;
+            double bc = 120 / 255.0;
+
+            Color miColor = new Color();
+            int r = 0;
+            int g = 0;
+            int b = 0;
+
+            // Creamos la omagen en tonos de gris
+            tonosDeGrisToolStripMenuItem_Click(sender, e);
+
+            for(x = 0; x < original.Width; x++)
+            {
+                for (y = 0; y < original.Height; y++)
+                {
+                    miColor = resultante.GetPixel(x, y);
+
+                    r = (int)(miColor.R * rc);
+                    g = (int)(miColor.G * gc);
+                    b = (int)(miColor.B * bc);
+
+                    resultante.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+            this.Invalidate();
+        }
+
+        private void colonizarGradianteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            float r1 = 120;
+            float g1 = 230;
+            float b1 = 120;
+
+            float r2 = 230;
+            float g2 = 100;
+            float b2 = 230;
+
+            int r = 0;
+            int g = 0;
+            int b = 0;
+
+            float dr = (r2 - r1) / original.Width;
+            float dg = (g2 - g1) / original.Width;
+            float db = (b2 - b1) / original.Width;
+
+            int x = 0;
+            int y = 0;
+
+            Color oColor;
+            //Color rColor
+
+            // Obtenemos los tonos de gris 
+            tonosDeGrisToolStripMenuItem_Click(sender, e);
+
+            for (x = 0; x < original.Width; x++)
+            {
+                for (y = 0; y < original.Height; y++)
+                {
+                    // Obtenemos el color 
+                    oColor = resultante.GetPixel(x, y);
+
+                    // Calculamos el color
+                    r = (int)((r1 / 255.0f) * oColor.R);
+                    g = (int)((g1 / 255.0f) * oColor.G);
+                    b = (int)((b1 / 255.0f) * oColor.B);
+
+                    if (r > 255)
+                    {
+                        r = 255;
+                    }
+                    else if (r < 0)
+                    {
+                        r = 0;
+                    }
+                    else if (g > 255)
+                    {
+                        g = 255;
+                    }
+                    else if (g < 0)
+                    {
+                        g = 0;
+                    }
+                    else if (b > 255)
+                    {
+                        b = 255;
+                    }
+                    else if (b < 0)
+                    {
+                        b = 0;
+                    }
+
+                    // Colocamos el color
+                    resultante.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+                // Avanzamos el color
+                r1 = (r1 + dr);
+                g1 = (g1 + dg);
+                b1 = (b1 + db);
+            }
+
+            this.Invalidate();
+
+        }
+
+        private void brilloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Modificamos el brillo de la imagen
+
+            int brillo = 64;  // El valor vca de -255 a 255
+            float pBrillo = 1.2f; // El valor vca de 0 a 2
+
+            int x = 0;
+            int y = 0;
+            resultante = new Bitmap(original.Width, original.Height);
+            Color rColor = new Color();
+            Color oColor = new Color();
+
+
+            int r = 0;
+            int g = 0;
+            int b = 0;
+
+
+            for (x = 0; x < original.Width; x++)
+            {
+                for (y = 0; y < original.Height; y++)
+                {
+                    // Obtenemos el color del pixel
+                    oColor = original.GetPixel(x, y);
+
+                    // Metodo 
+                    r = (int)(oColor.R * pBrillo);
+                    g = (int)(oColor.G * pBrillo);
+                    b = (int)(oColor.B * pBrillo);
+
+                    if (r > 255) r = 255;
+                    else if (r < 0) r = 0;
+
+                    if (g > 255) g = 255;
+                    else if (g < 0) g = 0;
+
+                    if (b > 255) b = 255;
+                    else if (b < 0) b = 0;
+
+                    rColor = Color.FromArgb(r, g, b);
+
+                    // Colocamos el color en resultante
+                    resultante.SetPixel(x, y, rColor);
+                }
+            }
+            this.Invalidate();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // creamos el bitmapa resultante del cuadro
+            resultante = new Bitmap(800, 600);
+
+            // Colocamos los valores para el dibujo con scrolls
+            anchoVentana = 800;
+            altoVentana = 600;
+
+        }
+    }
+}
